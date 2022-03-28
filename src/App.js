6ec824bdc22b722f58889fcb0f53b5a8f7c2126f
Sandeep-FED <sandymankara11@gmail.com>
2022-03-28 13:57:24 +0530

    map function with keys

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './style.css';
 import Prices from './prices.js';
 import Product from './product.js';
+import Keys from './keys.js';
 export default function App() {
   return (
     <div>
       {/* <Prices /> */}
       {/* <Product /> */}
+      <Keys/>
     </div>
   );
 }
--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,18 @@
-export default function keys(){
-  return(
-    <div></div>
-  )
+import React from 'react';
+export default function Keys() {
+  const gadgetList = [
+    { id: 1, name: 'watch', price: '14000' },
+    { id: 2, name: 'smartphone', price: '72900' },
+    { id: 3, name: 'tablet', price: '46000' },
+  ];
+  return (
+    <div>
+      <h1>Keys</h1>
+      {gadgetList.map((list) => (
+        <li key={list}>
+          {list.name}: {list.price} inr
+        </li>
+      ))}
+    </div>
+  );
 }
